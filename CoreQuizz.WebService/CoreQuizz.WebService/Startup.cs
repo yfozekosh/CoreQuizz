7a524f5cea738c52d70f464083d49dd62768d9db
Yurii.Fozekosh <yurii.fozekosh@eleks.com>
2017-09-04 17:52:40 +0300

    added jwt

--- CoreQuizz.WebService/CoreQuizz.WebService/Startup.cs
+++ CoreQuizz.WebService/CoreQuizz.WebService/Startup.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using CoreQuizz.BAL.Contracts;
 using CoreQuizz.BAL.Managers.Extensions;
 using CoreQuizz.DataAccess.Extensions;
@@ -16,6 +18,8 @@ using Microsoft.IdentityModel.Tokens;
 using NLog.Extensions.Logging;
 using NLog.Web;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace CoreQuizz.WebService
 {
@@ -30,6 +34,7 @@ namespace CoreQuizz.WebService
                            .SetBasePath(env.ContentRootPath)
                            .AddJsonFile("appconfig.json", optional: true, reloadOnChange: true);
 
+
             Configuration = builder.Build();
         }
 
@@ -37,31 +42,6 @@ namespace CoreQuizz.WebService
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<AppConfig>(Configuration);
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.RequireHttpsMetadata = false;
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        // укзывает, будет ли валидироваться издатель при валидации токена
-                        ValidateIssuer = true,
-                        // строка, представляющая издателя
-                        ValidIssuer = AuthOptions.ISSUER,
-
-                        // будет ли валидироваться потребитель токена
-                        ValidateAudience = true,
-                        // установка потребителя токена
-                        ValidAudience = AuthOptions.AUDIENCE,
-                        // будет ли валидироваться время существования
-                        ValidateLifetime = true,
-
-                        // установка ключа безопасности
-                        IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
-                        // валидация ключа безопасности
-                        ValidateIssuerSigningKey = true,
-                    };
-                });
-
 
             services.AddDAL(Configuration);
 
@@ -93,11 +73,49 @@ namespace CoreQuizz.WebService
             app.UseBrowserLink();
             //}
 
+            string secretKey = "CoreQuizz_key_9352_{wq[[SS]]ds,,.!!";
+            var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey));
+            var options = new TokenProviderOptions
+            {
+                Audience = "ExampleAudience",
+                Issuer = "ExampleIssuer",
+                SigningCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256),
+            };
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                // The signing key must match!
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingKey,
+
+                // Validate the JWT Issuer (iss) claim
+                ValidateIssuer = true,
+                ValidIssuer = "ExampleIssuer",
+
+                // Validate the JWT Audience (aud) claim
+                ValidateAudience = true,
+                ValidAudience = "ExampleAudience",
+
+                // Validate the token expiry
+                ValidateLifetime = true,
+
+                // If you want to allow a certain amount of clock drift, set that here:
+                ClockSkew = TimeSpan.Zero
+            };
+            app.UseMiddleware<TokenProviderMiddleware>(Options.Create(options));
+
+            app.UseJwtBearerAuthentication(new JwtBearerOptions()
+            {
+                Audience = "https://localhost:44304/",
+                AutomaticAuthenticate = true,
+                TokenValidationParameters = tokenValidationParameters
+            });
+
             app.UseSession();
             app.UseStaticFiles();
 
             //app.UseIdentityServer(Configuration);
-            app.UseIdentity();
+            //app.UseIdentity();
             app.UseMvcWithDefaultRoute();
 
             if (env.IsDevelopment())
